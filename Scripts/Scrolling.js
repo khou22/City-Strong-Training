f36b010ff9e7d3f40fdb6c1e03cb268df2d48f3c
Kevin Hou <kevin.ch.hou@gmail.com>
2016-03-28 20:12:16 -0700

    Welcome section now fixed - content scrolls over and covers it

--- Scripts/Scrolling.js
+++ Scripts/Scrolling.js
@@ -1,10 +1,19 @@
 //JS file for scrolling to elements
 
 var scrollTo = function(elementID) {
-  console.log("Scrolling to element:", elementID); // Feedback
-  $(document).ready(function (){
-    $('html, body').animate({
-      scrollTop: $(elementID).offset().top - 70 // Subtract header height
-    }, 1000);
-  });
+  var animationTime = 750; // Set animation time (milliseconds)
+  // console.log("Scrolling to element:", elementID); // Feedback
+  if (elementID != '#welcome') { // If not the welcome section
+    $(document).ready(function (){ // Document is loaded
+      $('html, body').animate({
+        scrollTop: $(elementID).offset().top - 70 // Subtract header height
+      }, animationTime);
+    });
+  } else { // If the welcome section
+    $(document).ready(function (){ // Document is loaded
+      $('html, body').animate({
+        scrollTop: 0 // Just scroll to the top, the welcome section is static
+      }, animationTime);
+    });
+  }
 }
